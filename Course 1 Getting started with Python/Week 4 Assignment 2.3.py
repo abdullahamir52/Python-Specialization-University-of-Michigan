fa9a86111041f87a74be8b37d3e506a89b67ab1e
abdullahamir52 <abdullahamir52@gmail.com>
2020-07-05 23:21:45 +0600

    added the course 1 files 1

---
+++ Course 1 Getting started with Python/Week 4 Assignment 2.3.py
@@ -0,0 +1,13 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+hours = float(hrs)
+
+py = input ('Enter Pay:')
+pay = float (py)
+
+total = pay*hours
+
+print('Pay:', total)
+
+a = input('End')
